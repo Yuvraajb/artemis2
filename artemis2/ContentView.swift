e730125ea84226613814ec35c07b70a481a18502
yuvraaj <yuvraajbhatter10@gmail.com>
2026-02-10 23:37:35 -0800

    Initial Commit

---
+++ artemis2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  artemis2
+//
+//  Created by Yuvraaj Bhatter on 2/10/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ artemis2/artemis2App.swift
@@ -0,0 +1,17 @@
+//
+//  artemis2App.swift
+//  artemis2
+//
+//  Created by Yuvraaj Bhatter on 2/10/26.
+//
+
+import SwiftUI
+
+@main
+struct artemis2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
